3aa8be2486d0a6c80c555c81aa14b68b0d4e6b61
ejim11 <favourejim56@gmail.com>
2024-12-07 01:12:04 +0100

    🎨 created reusable button with arrow comp

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { RiArrowRightUpLine } from "react-icons/ri";
+
+const Button = ({ text, styles }: { text: string; styles: string }) => {
+  return (
+    <motion.button
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, ease: "easeIn", delay: 0.8 }}
+      className={`${styles} flex items-center text-white-1 font-inter bg-purple-2  rounded-[0.6rem] `}
+    >
+      <span>{text}</span>
+      <RiArrowRightUpLine className="text-current w-[2rem] h-[2rem] ml-[2rem]" />
+    </motion.button>
+  );
+};
+
+export default Button;
