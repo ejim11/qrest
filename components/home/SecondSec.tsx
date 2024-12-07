eb671e8e9fd282b1cddcbda82b3d3fdf8b8d756d
ejim11 <favourejim56@gmail.com>
2024-12-07 01:16:59 +0100

    🎨 created the second sec comp for the home page

---
+++ components/home/SecondSec.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+
+const SecondSec = () => {
+  const textStr = `We blend creativity with purpose, creating visuals that are as
+        functional as they are beautiful, ensuring that they contribute to the
+        overall success of the brand.`;
+  return (
+    <section className="bg-black-1 flex items-center justify-center py-[15rem]">
+      <p className="w-[65%] text-white-1 text-center font-dmsans text-[6.4rem] leading-[6.8rem] knockout">
+        {textStr
+          .slice()
+          .split("")
+          .map((text, i) => (
+            <span key={i}>{text}</span>
+          ))}
+      </p>
+    </section>
+  );
+};
+
+export default SecondSec;
