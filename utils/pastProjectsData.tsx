ee707848c2e2cc03b85a964d5d1bfd40284a45cd
ejim11 <favourejim56@gmail.com>
2024-12-07 01:18:04 +0100

    🗃️ added past project and clients data

---
+++ utils/pastProjectsData.tsx
@@ -0,0 +1,66 @@
+import { StaticImageData } from "next/image";
+import pastProject1 from "../assets/home/past-project1.png";
+import pastProject2 from "../assets/home/past-project2.png";
+import pastProject3 from "../assets/home/past-project3.png";
+import pastProject4 from "../assets/home/past-project4.png";
+import pastProject5 from "../assets/home/past-project5.png";
+
+export const pastProjectsData: {
+  image: StaticImageData;
+  title: string;
+  text: string;
+}[] = [
+  {
+    image: pastProject1,
+    title: "project 1",
+    text: "branding",
+  },
+  {
+    image: pastProject2,
+    title: "project 2",
+    text: "web development",
+  },
+  {
+    image: pastProject3,
+    title: "project 3",
+    text: "web design",
+  },
+  {
+    image: pastProject4,
+    title: "project 4",
+    text: "mobile development",
+  },
+  {
+    image: pastProject5,
+    title: "project 5",
+    text: "web design",
+  },
+];
+
+export const hearFromClients = [
+  {
+    title: "They’ve exceeded my expectations",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    clientName: "Okoye Chinedu",
+  },
+  {
+    title: "They’ve exceeded my expectations",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    clientName: "Ahmed Johnson",
+  },
+  {
+    title: "They’ve exceeded my expectations",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    clientName: "Sophia Rasheed",
+  },
+  {
+    title: "They’ve exceeded my expectations",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    clientName: "Desmond Nwaeze",
+  },
+  {
+    title: "They’ve exceeded my expectations",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    clientName: "Agu john",
+  },
+];
