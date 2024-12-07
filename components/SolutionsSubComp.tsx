ba213220bb91117e9f4283a23e0d1eff9cf83204
ejim11 <favourejim56@gmail.com>
2024-12-07 01:13:37 +0100

    created the solutions modal

---
+++ components/SolutionsSubComp.tsx
@@ -0,0 +1,100 @@
+"use client";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import adAnnouncementIcon from "../assets/home/ad-announcement-megaphone 1.svg";
+import penToolIcon from "../assets/home/pen-tool 1.svg";
+import tvIcon from "../assets/home/monitor 1.svg";
+import codeIcon from "../assets/home/code 2.svg";
+
+const SolutionsSubComp = () => {
+  const [isMouseOver, setIsMouseOver] = useState<number | null>(null);
+
+  const solutionsList: {
+    title: string;
+    text: string;
+    icon: StaticImageData;
+  }[] = [
+    {
+      title: "Branding & Design",
+      text: "Crafting unique and memorable brand identities that resonate with your target audience.",
+      icon: penToolIcon,
+    },
+    {
+      title: "UIUX Design",
+      text: "Creating seamless user experiences that improve engagement and satisfaction across digital platforms.",
+      icon: tvIcon,
+    },
+    {
+      title: "Software Development",
+      text: "Developing tailored mobile apps and websites that meet your specific business needs.",
+      icon: codeIcon,
+    },
+    {
+      title: "Digital Marketing",
+      text: "Implementing strategic marketing solutions to enhance your online presence and drive growth.",
+      icon: adAnnouncementIcon,
+    },
+  ];
+
+  return (
+    <motion.ul
+      initial={{ opacity: 0, y: -40 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -40 }}
+      className="absolute top-[7.2rem] bg-white-1 w-[43.4rem] px-[1rem] py-[1rem]  rounded-[0.6rem]  text-black-1 shadow-2xl"
+    >
+      {solutionsList.map(
+        (item: { title: string; text: string; icon: StaticImageData }, i) => (
+          <motion.li
+            initial={{ opacity: 0, y: 20, x: 20 }}
+            whileInView={{ opacity: 1, y: 0, x: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              delay: (i + 1) * 0.1,
+              type: "spring",
+              stiffness: 300,
+              damping: 24,
+            }}
+            // variants={itemVariants}
+            key={i}
+            onMouseOver={() => {
+              setIsMouseOver(i);
+            }}
+            onMouseLeave={() => {
+              setIsMouseOver(null);
+            }}
+            className="flex px-[1.4rem] py-[1.5rem] rounded-[0.6rem]   hover:bg-purple-4 "
+          >
+            <Image
+              src={item.icon}
+              alt={item.title}
+              priority
+              width={50}
+              height={50}
+              className="w-[2.8rem] h-[2.8rem] mr-[2rem]"
+            />
+            <div className="flex flex-col text-left font-dmsans ">
+              <p
+                className={`${
+                  isMouseOver === i ? " text-purple-2" : "text-black-2 "
+                } text-[1.8rem] font-medium`}
+              >
+                {item.title}
+              </p>
+              <p
+                className={`${
+                  isMouseOver === i ? " text-purple-2" : "text-grey-1 "
+                } text-[1.4rem] leading-[2rem]`}
+              >
+                {item.text}
+              </p>
+            </div>
+          </motion.li>
+        )
+      )}
+    </motion.ul>
+  );
+};
+
+export default SolutionsSubComp;
