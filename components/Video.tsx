e352b3129686a1f318fd7916b083acd79bb82863
ejim11 <favourejim56@gmail.com>
2024-12-11 16:29:04 +0100

    ✅ added the video component

---
+++ components/Video.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <video width="1200" height="720" className="w-full" autoPlay muted>
+      <source src="home.mp4" type="video/mp4" />
+    </video>
+  );
+}
