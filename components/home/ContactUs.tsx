5557a3abd53eeea568c7bd3808c2b5f1a3ae1940
ejim11 <favourejim56@gmail.com>
2024-12-12 19:09:07 +0100

    ♻️ refactored and made responsive

--- components/home/ContactUs.tsx
+++ components/home/ContactUs.tsx
@@ -4,40 +4,40 @@ import Button from "../Button";
 import Image from "next/image";
 import contactUsImg from "../../assets/home/contact-us.png";
 import { motion } from "framer-motion";
+import SectionCard from "../SectionCard";
+import HeaderText from "../HeaderText";
 
 const ContactUs = () => {
   return (
-    <section className="bg-black-1 px-[9rem] py-[14rem] flex items-center">
-      <div className="flex-1 mr-[8rem]">
-        <motion.h3
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.3, ease: "easeIn" }}
-          className="font-inter font-semibold text-[6.8rem] text-white-1 leading-[7.1rem] -tracking-[0.5rem]"
-        >
-          Got an Idea? Let’s Make It Happen!
-        </motion.h3>
+    <SectionCard styles="bg-black-1  py-[14rem] smd:py-[8rem] md:py-[12rem]  flex items-center md:flex-col">
+      <div className="flex-1 mr-[8rem] md:mr-0 md:flex md:flex-col md:items-center md:mb-[4rem]">
+        <HeaderText
+          text="Got an Idea? Let’s Make It Happen!"
+          styles="text-white-1 font-inter md:text-center"
+        />
         <motion.p
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
           transition={{ duration: 0.3, ease: "easeIn" }}
-          className="mt-[2rem] text-[2rem] font-dmsans leading-[2.9rem] text-white-3 mb-[3.2rem]"
+          className=" text-[2rem] sm:text-[1.5rem] sm:leading-[2.2rem] font-dmsans md:text-center leading-[2.9rem] text-white-3 mb-[3.2rem]"
         >
           We combine creativity, expertise, and a collaborative approach to
           ensure that your project is not only successful but also exceeds your
           expectations. Let’s embark on this journey together and turn your
           dreams into tangible results.{" "}
         </motion.p>
-        <Button text={"Contact us"} styles={"py-[1.4rem] px-[2.8rem]"} />
+        <Button
+          text={"Contact us"}
+          styles={"py-[1.4rem] px-[2.8rem] smd:w-full smd:justify-between"}
+        />
       </div>
       <motion.div
         initial={{ opacity: 0, x: 40 }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         transition={{ duration: 0.3, ease: "easeIn" }}
-        className="w-[67.3rem] h-[71.6] flex-1"
+        className="w-[67.3rem] smd:w-full h-[71.6] flex-1"
       >
         <Image
           src={contactUsImg}
@@ -48,7 +48,7 @@ const ContactUs = () => {
           className="w-full h-full"
         />
       </motion.div>
-    </section>
+    </SectionCard>
   );
 };
 
