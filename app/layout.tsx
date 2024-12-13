2652585d6f1cc3ff8de18c2c14fc6219b28adc38
ejim11 <favourejim56@gmail.com>
2024-12-13 09:00:33 +0100

    💄 added overflow hidden to the container

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${dmSans.variable} `}>
-      <body>
+      <body className="overflow-x-hidden">
         <AppContextProvider>
           <Header />
           {children}
