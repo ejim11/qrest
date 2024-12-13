5955fd9c0104876e98e02bbd984cb9ae6314a2b6
ejim11 <favourejim56@gmail.com>
2024-12-13 09:01:30 +0100

    ♻️ refactored the headertext comp

--- components/home/ContactUs.tsx
+++ components/home/ContactUs.tsx
@@ -14,29 +14,23 @@ const ContactUs = () => {
         <HeaderText
           text="Got an Idea? Let’s Make It Happen!"
           styles="text-white-1 font-inter md:text-center"
-        />
-        <motion.p
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.3, ease: "easeIn" }}
-          className=" text-[2rem] sm:text-[1.5rem] sm:leading-[2.2rem] font-dmsans md:text-center leading-[2.9rem] text-white-3 mb-[3.2rem]"
-        >
-          We combine creativity, expertise, and a collaborative approach to
+          paragraph="We combine creativity, expertise, and a collaborative approach to
           ensure that your project is not only successful but also exceeds your
           expectations. Let’s embark on this journey together and turn your
-          dreams into tangible results.{" "}
-        </motion.p>
+          dreams into tangible results."
+          paragraphStyle=" text-white-3 mb-[3.2rem] md:text-center"
+        />
+
         <Button
           text={"Contact us"}
           styles={"py-[1.4rem] px-[2.8rem] smd:w-full smd:justify-between"}
         />
       </div>
       <motion.div
-        initial={{ opacity: 0, x: 40 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.3, ease: "easeIn" }}
+        // initial={{ opacity: 0, x: 40 }}
+        // whileInView={{ opacity: 1, x: 0 }}
+        // viewport={{ once: true }}
+        // transition={{ duration: 0.3, ease: "easeIn" }}
         className="w-[67.3rem] smd:w-full h-[71.6] flex-1"
       >
         <Image
