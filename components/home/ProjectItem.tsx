f80351ca749cae08f27508c52c8c3b6408bb8aa8
ejim11 <favourejim56@gmail.com>
2024-12-19 11:00:03 +0100

    💄 made image cover availabe space

--- components/home/ProjectItem.tsx
+++ components/home/ProjectItem.tsx
@@ -10,14 +10,17 @@ const ProjectItem = ({ project }: { project: any }) => {
       href={`our-work/#${project.slug}`}
       className="px-[1rem] block smd:px-0 ring-0 focus:ring-0 outline-none active:ring-0 active:outline-none focus:outline-none"
     >
-      <Image
-        src={project.image}
-        alt={`${project.title} image`}
-        priority
-        width={1000}
-        height={1000}
-        className="w-full h-[43.2rem] object-cover"
-      />
+      <div className="w-full h-[43rem] overflow-hidden bg-purple-1">
+        <Image
+          src={project.image}
+          alt={`${project.title} image`}
+          priority
+          width={1000}
+          height={1000}
+          className="w-full h-full object-cover"
+        />
+      </div>
+
       <p className="font-geist uppercase text-black-2 font-medium mb-[1.2rem] mt-[2.4rem]">
         {project.title}
       </p>
