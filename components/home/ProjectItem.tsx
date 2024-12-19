2fd832755c053271cc4d86ebc06611cdef41ea3f
ejim11 <favourejim56@gmail.com>
2024-12-19 07:10:42 +0100

    🎨 made the project item comp a link

--- components/home/ProjectItem.tsx
+++ components/home/ProjectItem.tsx
@@ -2,17 +2,21 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const ProjectItem = ({ project }: { project: any }) => {
   return (
-    <div className="px-[1rem] smd:px-0">
+    <Link
+      href={`our-work/#${project.slug}`}
+      className="px-[1rem] block smd:px-0 ring-0 focus:ring-0 outline-none active:ring-0 active:outline-none focus:outline-none"
+    >
       <Image
         src={project.image}
         alt={`${project.title} image`}
         priority
         width={1000}
         height={1000}
-        className="w-full h-[43.2rem]"
+        className="w-full h-[43.2rem] object-cover"
       />
       <p className="font-geist uppercase text-black-2 font-medium mb-[1.2rem] mt-[2.4rem]">
         {project.title}
@@ -20,7 +24,7 @@ const ProjectItem = ({ project }: { project: any }) => {
       <p className="text-grey-1 font-dmsans text-[2rem] sm:text-[1.5rem]">
         {project.text}
       </p>
-    </div>
+    </Link>
   );
 };
 
