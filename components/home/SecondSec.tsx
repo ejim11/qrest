eb969c503f51418376b746da1ba7eedf3b07bd94
ejim11 <favourejim56@gmail.com>
2025-01-04 22:12:23 +0100

    💫 changed the animation and removed dead code

--- components/home/SecondSec.tsx
+++ components/home/SecondSec.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import SectionCard from "../SectionCard";
+import { motion } from "framer-motion";
 
 const SecondSec = () => {
   const textStr = `We blend creativity with purpose, creating visuals that are as
@@ -8,14 +9,15 @@ const SecondSec = () => {
         overall success of the brand.`;
   return (
     <SectionCard styles="bg-black-1 flex items-center justify-center py-[15rem] md:py-[12rem] smd:py-[8rem]">
-      <p className="w-[90%] lg:w-full text-white-1 text-center font-dmsans text-[6.4rem] xlg:text-[6rem] xmd:text-[4.5rem] smd:text-[3rem] sm:text-[2.7rem] smd:leading-[3rem] leading-[6.8rem] knockout">
-        {textStr
-          .slice()
-          .split("")
-          .map((text, i) => (
-            <span key={i}>{text}</span>
-          ))}
-      </p>
+      <motion.p
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 100, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        viewport={{ once: true }}
+        className="w-[90%] lg:w-full text-white-2 text-center font-dmsans text-[6.4rem] xlg:text-[6rem] xmd:text-[4.5rem] smd:text-[3rem] sm:text-[2.7rem] smd:leading-[3rem] leading-[6.8rem] "
+      >
+        {textStr}
+      </motion.p>
     </SectionCard>
   );
 };
